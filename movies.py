17a939e2aa732e8603eedc87e043d9f6eda45b24
Luke Dosen <lddosen@gmail.com>
2018-11-16 19:15:28 -0600

    grid search working to my liking

--- movies.py
+++ movies.py
@@ -3,17 +3,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from collections import Counter
-from sklearn import metrics
+from sklearn.metrics import f1_score
 from statistics import mean
+import operator
 
 
 class Vectorizer(object):
 
-    def __init__(self, n=0, m=0):
-        self.n = n
+    def __init__(self, m=0):
         self.m = m
 
-    def extract_words(self, sentences, count_max, count_min):
+    def extract_words(self, sentences, count_min):
         words_raw = []
         for sentence in sentences:
             words_raw.extend(sentence.split())
@@ -25,13 +25,13 @@ class Vectorizer(object):
         words = []
         for word in word_counts:
             count = word_counts[word]
-            if count_max > count > count_min:
+            if 900 > count > count_min:
                 words.append(word)
 
         return words
 
     def fit_transform(self, sentences):
-        lexicon = self.extract_words(sentences, self.n, self.m)
+        lexicon = self.extract_words(sentences, self.m)
         featureset = []
 
         for w in sentences:
@@ -72,25 +72,29 @@ class K_Fold(object):
 
 class gridSearchTwo(object):
 
-    def __init__(self, model_params, vectorizer_params):
-        self.model_params = model_params # a dict containing and array of parameters
-        self.vectorizer_params = vectorizer_params # a dict containing and array of parameters
+    def __init__(self, model, vectorizer, param_grid):
+        self.model = model
+        self.vectorizer = vectorizer
+        self.param_grid = param_grid
 
     def fit(self, X, y):
 
-        all_scores = []
         k_fold = K_Fold()
+        score_params = {}
 
-        for i in range(len(self.vectorizer_params)):
-            vect = Vectorizer(self.vectorizer_params.get('n')[i], self.vectorizer_params.get('m')[i])
-            X_new = vect.fit_transform(X)
-            for j in range(len(self.model_params)):
-                model = LogisticRegression(C=self.model_params.get('C')[j])
-                scores = k_fold.cross_validation(model, X_new, y, 10)
-                all_scores.extend(mean(scores))
+        for j in range(len(self.param_grid.get('m'))):
+            vect = self.vectorizer(self.param_grid.get('m')[j])
+            m = str(self.param_grid.get('m')[j])
 
-        sorted_all_scores = sorted(all_scores)
-        return sorted_all_scores[0]
+            for i in range(len(self.param_grid.get('C'))):
+                lreg = self.model(C=self.param_grid.get('C')[i])
+                c = str(self.param_grid.get('C')[i])
+                scores = k_fold.cross_validation(lreg, vect.fit_transform(X), y, 10)
+                best_score = mean(scores)
+                mc = m + ", " + c
+                score_params.update({mc: best_score})
+
+        return score_params
 
 
 data = pd.read_csv('movie_reviews.csv')
@@ -99,17 +103,22 @@ data = pd.read_csv('movie_reviews.csv')
 # extract true class labels from features
 X, y = data.iloc[:, 0].values, data.iloc[:, 1].values
 
+vect = Vectorizer(10)
+X = vect.fit_transform(X)
+
 # split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1337)
 
-# transform the words in vectors
-vect = Vectorizer(900, 10)
-X_train = vect.fit_transform(X_train)
-X_test = vect.fit_transform(X_test)
+'''
+param_grid = {'C': [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0],
+              'm': [10, 15, 20, 25, 30, 35, 40]}
 
-lreg = LogisticRegression(C=0.1)
+gs = gridSearchTwo(LogisticRegression, Vectorizer, param_grid)
 
+scores = gs.fit(X_train, y_train)
+'''
+
+lreg = LogisticRegression(C=1.0)
 lreg.fit(X_train, y_train)
-score = lreg.score(X_train, y_train)
-
-
+score = lreg.predict(X_test)
+print("F1 Score:\n", (f1_score(y_test, score, average='macro')) * 100)
